df60f86443e6dd06ead336a48297a31c2b8ad0f4
Sadickachuli <m.achuli@alustudent.com>
2023-10-18 22:46:17 +0200

    done

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const images = document.querySelectorAll('.skills img, .experience img, .education img, .projects img');
     const textDivs = document.querySelectorAll('.text-box');
-    const scaleFactor = 1.28; // Adjust this value to control the zoom level
+    const scaleFactor = 1.28; // This controls the zoom level
     const isImageVisible = Array(images.length).fill(true);
   
     images.forEach((image, index) => {
       image.addEventListener('mouseenter', () => {
         if (isImageVisible[index]) {
-          image.style.borderRadius = '50%'; // Make the image circular
-          textDivs[index].style.display = 'none'; // Hide the text-box
-          image.style.transform = `scale(${scaleFactor})`; // Zoom in the image
-          image.style.transition = 'transform 0.3s'; // Add a smooth transition effect
+          image.style.borderRadius = '50%'; // Makes the image circular when the mouse hovers over it
+          textDivs[index].style.display = 'none'; // Hides the text-box
+          image.style.transform = `scale(${scaleFactor})`; // This will zoom in the image
+          image.style.transition = 'transform 0.3s'; // To add a smooth transition effect
         }
       });
   
